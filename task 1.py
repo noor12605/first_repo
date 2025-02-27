861efcdee6db53a39b887cd6b4ad51b52f06cbc7
Noor fatima <noorfatima12605@gmail.com>
2025-02-27 09:43:36 +0500

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,40 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # task no 1
+>>> 
+>>> #display several nmbrs
+>>> print(2)
+2
+>>> print(4)
+4
+>>> print("12,45,87")
+12,45,87
+>>> #solve and show the summation of 64+32
+>>> sum=64+32
+>>> print("the sum of 64 and 32 is:",sum)
+the sum of 64 and 32 is: 96
+>>> #same as 2 but in sum=x+y
+>>> x=64
+>>> y=32
+>>> sum=x+y
+>>> print("the sum of 64 and 32 is:",sum)
+the sum of 64 and 32 is: 96
+>>> #print'lucky' inside s
+>>> s='Lucky'
+>>> print(s)
+Lucky
+>>> #print day ,month ,year in format
+>>> day=27
+>>> month=02
+SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+>>> day=27
+>>> month=2
+>>> year=2025
+>>> print("Today is",day,"/",month,"/",year)
+Today is 27 / 2 / 2025
+>>> #ask user his/her phone nmbr
+>>> phone_number=input("Enter your phone number:")
+Enter your phone number:03418610360
+>>> print("Your phone number is:",phone_number)
+Your phone number is: 03418610360
+>>> 
