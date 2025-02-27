5d5e97fa87a357ada167cd0f2dce78873e3d5052
Noor fatima <noorfatima12605@gmail.com>
2025-02-27 12:59:08 +0500

    Add files via upload

---
+++ task no 3.py
@@ -0,0 +1,15 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> student_marks=float(input("enter your marks:"))
+enter your marks:67
+>>> if student_marks>=85:
+	print("grade is A")
+elif student_marks>=70:
+	print("grade is B")
+elif student_marks>=50:
+	print("grade is C")
+else:
+	print("grade is F")
+
+grade is C
+>>> 
